823d2a7a807e55bfb4c3219ce108e0f1bf051c08
Oleg_bul <kokseg63@gmail.com>
2022-12-21 12:27:05 +0400

    init commit

---
+++ Teachers_Candys_game.py
@@ -0,0 +1,38 @@
+import random
+candys = 117
+
+
+def input_candy():
+    global candys
+    while True:
+        move = int(input('Введите конфеты '))
+        if move > 0 and move < 29 and move <= candys:
+            candys -= move
+            break
+        else:
+            print('Столько взять нельзя')
+
+
+def bot_take():
+    global candys
+    move = random.randint(1, 28)
+    print(f'Бот взял {move}')
+    candys -= move
+
+
+print(f'На столе лежит {candys} конфет')
+players = ['Пользователь', "Компьютер"]
+move = random.choice(players)
+print(f'Первым ходит - {move}')
+while True:
+    if move == 'Пользователь':
+        input_candy()
+        print(f'Осталось {candys}')
+        move = "Компьютер"
+    else:
+        bot_take()
+        print(f'Осталось {candys}')
+        move = 'Пользователь'
+    if candys < 29:
+        print(f'Победил {move}')
+        break
