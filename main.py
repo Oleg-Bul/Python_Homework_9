03d86891a911afbc33c3807e42a6ec7656d099f5
Oleg_bul <kokseg63@gmail.com>
2022-12-24 14:48:37 +0400

    Final

--- main.py
+++ main.py
@@ -1,21 +1,74 @@
-# Напишите программу, удаляющую из сообщения,
-#  которое присл пользователь все слова,
-# содержащие "абв" и отправляет его обратно пользователю.
 import telebot
 import cnfg
-import model
+import random
+
 bot = telebot.TeleBot(cnfg.TOKEN)
 
+candys = dict()
+activ_player = dict()
+enable_game = dict()
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+
+def handle_game_proc(message):  # Честно: эту функцию  взял у вас)
+    global enable_game
+    try:
+        if enable_game[message.chat.id] and 1 <= int(message.text) <= 28:
+            return True
+        else:
+            return False
+    except KeyError:
+        enable_game[message.chat.id] = False
+        if enable_game[message.chat.id] and 1 <= int(message.text) <= 28:
+            return True
+        else:
+            return False
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    global candys, activ_player, enable_game
+    bot.reply_to(message, f'Добро пожаловать в игру "Конфеты"')
+    candys[message.chat.id] = 117
+    activ_player[message.chat.id] = random.choice(['Пользователь', "Бот"])
+    bot.send_message(message.chat.id, f'Начинает {activ_player[message.chat.id]}')
+    enable_game[message.chat.id] = True
+    if activ_player[message.chat.id] == 'Бот':
+        bot_take = random.randint(1, 28)
+        candys[message.chat.id] -= bot_take
+        bot.send_message(message.chat.id, f'Бот взял {bot_take}')
+        bot.send_message(message.chat.id,
+                         f'Осталось {candys[message.chat.id]} конфет')
+        activ_player[message.chat.id] = 'Пользователь'
+
+@bot.message_handler(commands=["help"])
+def help(message):
+	bot.send_message(message.chat.id, f'Условие игры: На столе лежит 117 конфет.\
+		Играют два игрока делая ход друг после друга. \
+		Первый ход определяется жеребьёвкой.\
+		За один ход можно забрать не более чем 28 конфет.\
+		Все конфеты оппонента достаются сделавшему последний ход.')
 
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, model.abc_delete(message.text))
-
+@bot.message_handler(func=handle_game_proc)
+def game(message):
+    global candys, activ_player, enable_game
+    if activ_player[message.chat.id] == 'Пользователь':
+        if candys[message.chat.id] > 28:
+            candys[message.chat.id] -= int(message.text)
+            bot.send_message(message.chat.id, f'На столе {candys[message.chat.id]} конфет')
+            if candys[message.chat.id] > 28:
+                bot_take = random.randint(1, 28)
+                candys[message.chat.id] -= bot_take
+                bot.send_message(message.chat.id, f'Бот взял {bot_take}')
+                bot.send_message(message.chat.id, f'На столе {candys[message.chat.id]} конфет')
+                if candys[message.chat.id] <= 28:
+                    bot.send_message(message.chat.id, 'Вы выиграли')
+                    enable_game[message.chat.id] = False
+            else:
+                bot.send_message(message.chat.id, 'Бот выиграл')
+                enable_game[message.chat.id] = False
+    else:
+        bot.send_message(message.chat.id, 'Бот выиграл')
+        enable_game[message.chat.id] = False
 
 print('server run')
 bot.infinity_polling()
--- model.py
+++
@@ -1,6 +0,0 @@
-
-
-def abc_delete(message):
-    find_txt = 'абв'
-    lst = [i for i in message.split() if find_txt not in i]
-    return "".join(lst)
